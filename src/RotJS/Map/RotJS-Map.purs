39d4d5af4e70227c2c8c2be01c4f58ad832abf8e
Martin Galese <martin@galese.net>
2016-06-26 13:02:48 -0400

    cleanup map ffi to remove unused stuff

--- src/RotJS/Map/RotJS-Map.purs
+++ src/RotJS/Map/RotJS-Map.purs
@@ -1,15 +1,8 @@
-module RotJS.Map (RNG, buildArena) where
+module RotJS.Map (buildArena) where
 
 import Prelude
 import Control.Monad.Eff (Eff)
 import Data.Function.Uncurried (Fn2, runFn2)
-
-
--- | `TTY` effect denotes computations which manipulate the ROT.js Display`
-foreign import data RNG :: !
-
--- | ROT.js mapgen context
-foreign import data MapGen :: *
 
 foreign import buildArenaRaw :: Fn2 Int Int (Array Int)
 
