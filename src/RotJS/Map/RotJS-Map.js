cd1486aea27a6f98e434c7b510a951a63a4b159c
Martin Galese <martin@galese.net>
2016-06-26 14:38:50 -0400

    more map types, using RotJS.RNG eff

--- src/RotJS/Map/RotJS-Map.js
+++ src/RotJS/Map/RotJS-Map.js
@@ -2,15 +2,40 @@
 
 var ROT = require('rot-js');
 
-exports.buildArenaRaw = function (xsize, ysize) {
-    var map = new ROT.Map.Arena(xsize, ysize);
+var mapExport = function (map) {
     var mapStorage = [];
-
+    var xsize = map._width;
+    var ysize = map._height;
     var callback = function(x, y, value) {
       var key = x * ysize + y;
       mapStorage[key] = value;
     }
-
     map.create(callback);
     return mapStorage;
+  }
+
+exports.buildArenaRaw = function (xsize, ysize) {
+  return function() {
+    var map = new ROT.Map.Arena(xsize, ysize);
+    return mapExport(map);
+  };
+};
+
+exports.buildDiggerRaw = function(xsize, ysize) {
+  return function() {
+    var map = new ROT.Map.Digger(xsize, ysize);
+    return mapExport(map);
+  };
+};
+
+exports.buildUniformRaw = function(xsize, ysize) {
+  return function() {
+    var map = new ROT.Map.Uniform(xsize, ysize);
+    return mapExport(map);
+  };
+};
+
+exports.buildRogueRaw = function(xsize, ysize) {
+  var map = new ROT.Map.Rogue(xsize, ysize);
+  return mapExport(map);
 };
--- src/RotJS/Map/RotJS-Map.purs
+++ src/RotJS/Map/RotJS-Map.purs
@@ -1,10 +1,28 @@
-module RotJS.Map (buildArena) where
+module RotJS.Map (arena, digger, uniform, rogue) where
 
 import Prelude
 import Control.Monad.Eff (Eff)
 import Data.Function.Uncurried (Fn2, runFn2)
 
-foreign import buildArenaRaw :: Fn2 Int Int (Array Int)
+import RotJS.RNG as Random
 
-buildArena :: Int -> Int -> Array Int
-buildArena = runFn2 buildArenaRaw
+type Map = Array Int
+
+foreign import buildArenaRaw :: Fn2 Int Int (Map)
+foreign import buildDiggerRaw :: forall eff. Fn2 Int Int (Eff (rotrng :: Random.RNG | eff) Map)
+foreign import buildUniformRaw :: forall eff. Fn2 Int Int (Eff (rotrng :: Random.RNG | eff) Map)
+foreign import buildRogueRaw :: forall eff. Fn2 Int Int (Eff (rotrng :: Random.RNG | eff) Map)
+
+
+
+arena:: Int -> Int -> Map
+arena = runFn2 buildArenaRaw
+
+digger :: forall eff. Int -> Int -> Eff (rotrng :: Random.RNG | eff) Map
+digger = runFn2 buildDiggerRaw
+
+uniform :: forall eff. Int -> Int -> Eff (rotrng :: Random.RNG | eff) Map
+uniform = runFn2 buildUniformRaw
+
+rogue :: forall eff. Int -> Int -> Eff (rotrng :: Random.RNG | eff) Map
+rogue = runFn2 buildRogueRaw
